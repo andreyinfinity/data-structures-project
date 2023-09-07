134f052ead556e9177664c67e77083a484cb7aff
Andrey Potapov <aapot@ya.ru>
2023-09-07 22:12:11 +0300

    Добавлен класс LinkedList с методами добавления в начало и конец списка

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,30 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+    def __init__(self, data, next_node):
+        self.data = data
+        self.next_node = next_node
 
 
 class LinkedList:
     """Класс для односвязного списка"""
 
+    def __init__(self):
+        self.head = None
+
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        self.head = Node(data, self.head)
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        new_node = Node(data, None)
+        if self.head is None:
+            self.head = new_node
+        else:
+            node = self.head
+            while node.next_node is not None:
+                node = node.next_node
+            node.next_node = new_node
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
